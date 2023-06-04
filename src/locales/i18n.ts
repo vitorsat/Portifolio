816144ae912ce21dbd3668f9933885f88b997d7f
VitorMiguel <vitor.miguel0581@gmail.com>
2023-06-03 23:14:04 -0300

    feat: fix translate

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,11 +1,15 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector';
+
 
 import en from './en-us.json'
 import pt from './pt-br.json'
 
-i18n.use(initReactI18next).init({
+i18n
+.use(LanguageDetector)
+.use(initReactI18next).init({
   resources: {
     en: {
       ...en
@@ -13,8 +17,7 @@ i18n.use(initReactI18next).init({
     pt: {
       ...pt
     }
-  },
-  lng: 'pt'
+  }
 })
 
 export default i18n
