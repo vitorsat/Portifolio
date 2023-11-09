f2fd012da8e6462429db2a2af408b313c289179e
Vitor Saturnino <vitor.miguel0581@gmail.com>
2023-11-09 16:56:30 -0300

    Delete unused files and update dependencies.

---
+++ src/consts/globalConst.ts
@@ -0,0 +1,9 @@
+export const PT = "pt";
+export const EN = "en";
+export const LinkedIN = "https://www.linkedin.com/in/vitorsat/";
+export const Github = "https://github.com/vitorsat";
+export const SvgEuaFlag =
+  "http://purecatamphetamine.github.io/country-flag-icons/3x2/US.svg";
+export const SvgBrFlag =
+  "http://purecatamphetamine.github.io/country-flag-icons/3x2/BR.svg";
+export const YoutubeBaseVideoUrl = "https://www.youtube.com/@junaidusman4376";
---
+++ src/styles/Home/styles.ts
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+export const VideoStyled = styled.video`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+
+  @media (min-aspect-ratio: 16/9) {
+    width: 100%;
+    height: auto;
+  }
+
+  @media (max-aspect-ratio: 16/9) {
+    width: auto;
+    height: 100%;
+  }
+`;
+
+export const MainContainer = styled.main`
+  position: relative;
+  background: linear-gradient(rgba(12, 3, 51, 0.3), rgba(12, 3, 51, 0.3));
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100%;
+  text-align: center;
+  gap: 1rem;
+`;
+
+export const Header = styled.header`
+  position: absolute;
+  top: 0;
+  right: 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  padding: 1rem;
+`;
+
+export const ContainerInfos = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  padding: 0px 30px;
+`;
+
+export const IconAlertContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  p: {
+    fontweight: bold;
+    padding: 0;
+    border: 0;
+  }
+`;
