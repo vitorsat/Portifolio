a17ced66766d5d52baa62e7df11f0bb03c7cb056
Vitor Saturnino <vitor.miguel0581@gmail.com>
2023-11-09 17:13:03 -0300

    Update code formatting and styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,17 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { Analytics } from "@vercel/analytics/react";
-import { useEffect, useRef, useState } from "react";
-import {
-  GithubLogo,
-  LinkedinLogo,
-  Pause,
-  Play,
-  WarningOctagon,
-} from "phosphor-react";
-import {
-  EN,
-  Github,
-  PT,
-  SvgBrFlag,
-  SvgEuaFlag,
-  LinkedIN,
-  YoutubeBaseVideoUrl,
-} from "@/consts/globalConst";
-import {
-  ContainerInfos,
-  Header,
-  IconAlertContainer,
-  MainContainer,
-  VideoStyled,
-} from "../../styles/Home/styles";
-import { getLanguage } from "@/locales/languages";
+import Image from 'next/image';
+import { Analytics } from '@vercel/analytics/react';
+import { useEffect, useRef, useState } from 'react';
+import { GithubLogo, LinkedinLogo, Pause, Play, WarningOctagon } from 'phosphor-react';
+import { EN, Github, PT, SvgBrFlag, SvgEuaFlag, LinkedIN, YoutubeBaseVideoUrl } from '@/consts/globalConst';
+import { ContainerInfos, Header, IconAlertContainer, MainContainer, VideoStyled } from '../../styles/Home/styles';
+import { getLanguage } from '@/locales/languages';
 
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
-  const [language, setLanguage] = useState("pt");
+  const [language, setLanguage] = useState('pt');
 
   useEffect(() => {
     setLanguage(navigator.language);
@@ -62,49 +42,21 @@ export default function Home() {
   return (
     <>
       <Analytics />
-      <audio
-        src={"/resources/song.mp3"}
-        ref={audioRef}
-        typeof="audio/mp3"
-        loop
-      />
-      <VideoStyled
-        src={"/resources/video.mp4"}
-        autoPlay
-        muted
-        loop
-        id="myVideo"
-        typeof="video/mp4"
-      />
+      <audio src={'/resources/song.mp3'} ref={audioRef} typeof="audio/mp3" loop />
+      <VideoStyled src={'/resources/video.mp4'} autoPlay muted loop id="myVideo" typeof="video/mp4" />
       <MainContainer>
         <Header>
-          <Image
-            onClick={() => handleChangeLanguage(PT)}
-            alt="Brasil"
-            src={SvgBrFlag}
-            width="30"
-            height="30"
-          />
-          <Image
-            onClick={() => handleChangeLanguage(EN)}
-            alt="United States"
-            src={SvgEuaFlag}
-            width="30"
-            height="30"
-          />
-          {isPlaying ? (
-            <Pause size={32} onClick={togglePlay} />
-          ) : (
-            <Play size={32} onClick={togglePlay} />
-          )}
+          <Image onClick={() => handleChangeLanguage(PT)} alt="Brasil" src={SvgBrFlag} width="30" height="30" />
+          <Image onClick={() => handleChangeLanguage(EN)} alt="United States" src={SvgEuaFlag} width="30" height="30" />
+          {isPlaying ? <Pause size={32} onClick={togglePlay} /> : <Play size={32} onClick={togglePlay} />}
         </Header>
         <ContainerInfos>
           <IconAlertContainer>
-            <WarningOctagon size={32} style={{ color: "red" }} />
-            <p>{getLanguage(language)["developing"]}</p>
+            <WarningOctagon size={32} style={{ color: 'red' }} />
+            <p>{getLanguage(language)['developing']}</p>
           </IconAlertContainer>
           <h1>Vitor Saturnino</h1>
-          <p>{getLanguage(language)["main.title"]}</p>
+          <p>{getLanguage(language)['main.title']}</p>
           <div>
             <LinkedinLogo size={50} onClick={() => window.open(LinkedIN)} />
             <GithubLogo size={50} onClick={() => window.open(Github)} />
--- src/styles/Home/styles.ts
+++ src/styles/Home/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const VideoStyled = styled.video`
   width: 100%;
